7487a35181d7631b424a59b743fc48f3f153ed82
KyBineS <s_kalg@icloud.com>
2023-04-04 18:36:19 +0300

    Commit - 1

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,14 @@
 "use strict";
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     let helloMessage = 'Hello Incubator!!!';
     res.send(helloMessage);
+});
+app.get('/mans', (req, res) => {
+    let a = 'stupid mans';
+    res.send(a);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
